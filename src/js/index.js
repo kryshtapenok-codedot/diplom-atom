569f468e6ac54bddac2cd14e24ceef802ad82c34
Yaroslav_Kryshtapenok <kryshtapenok.codedot@gmail.com>
2021-05-31 17:32:58 +0300

    Внесение изменний в slider на странице index

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,53 @@
 var swiper = new Swiper('.swiper-container', {
-    slidesPerView: 4,
+  slidesPerView: 1.2,
+  slideActiveClass:'active',
+  breakpoints: {
+  1800:{
+    slidesPerView:6.8,
+  },
+  1600:{
+  slidesPerView: 5,
+    },
+  1200:{
+    slidesPerView:5,
+
+  },
+  800:{
+
+    slidesPerView:4,
+  },
+  600:{
+
+    slidesPerView:2.5,
+  },
+  440:{
+    slidesPerView:2
+  },
+  330:{
+    slidesPerView:1.3
+  }
+  
+  },
     autoplay: {
       delay: 3000,
       disableOnInteraction: false,
     },
-    spaceBetween: -600,
+
     loop:true,
+    loopFillGroupWithBlank:true,
     speed: 2000,
-    centeredSlides:true
+    centeredSlides:true,
+    slideActiveClass:'active'
+  });
+
+
+  $(function() {
+    $('.marquee').marquee({
+      duration: 18000,
+      startVisible: true,
+      duplicated: true,
+      delayBeforeStart:0,
+      pauseOnHover:true,
+      startVisible:true
+    });
   });
